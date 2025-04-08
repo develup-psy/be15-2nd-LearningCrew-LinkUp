8722fc78039ae194288d9aaf4da32095b3c07683
PSY <sksssd234@naver.com>
2025-04-09 03:33:24 +0900

    feat: Spring Security 인가/인증 및 jwt 필터 추가 설정 변경

--- linkup/src/main/java/com/learningcrew/linkup/config/SecurityConfig.java
+++ linkup/src/main/java/com/learningcrew/linkup/config/SecurityConfig.java
@@ -1,21 +1,34 @@
 package com.learningcrew.linkup.config;
 
+import com.learningcrew.linkup.security.jwt.JwtAuthenticationFilter;
+import com.learningcrew.linkup.security.jwt.JwtTokenProvider;
+import com.learningcrew.linkup.security.jwt.RestAccessDeniedHandler;
+import com.learningcrew.linkup.security.jwt.RestAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserDetailsService userDetailsService;
+    private final RestAccessDeniedHandler restAccessDeniedHandler;
+    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
     @Bean
     public PasswordEncoder passwordEncoder() {return new BCryptPasswordEncoder();}
 
@@ -24,15 +37,29 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(exception ->
+                    exception
+                            .authenticationEntryPoint(restAuthenticationEntryPoint)     // 인증 실패
+                            .accessDeniedHandler(restAccessDeniedHandler)          // 인가 실패
+                )
+                // 요청 http method, url 기준으로 인증, 인가 필요 여부 설정
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers(HttpMethod.POST, "/api/v1/users/register", "/api/v1/auth/login").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/v1/users/me").hasAuthority("USER") //추후에 hasRole로 수정
                                 .requestMatchers(HttpMethod.GET, "/api/v1/admin").hasAuthority("ADMIN")
                                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                                 .anyRequest().authenticated()
-                );
+                )
+                // 커스텀 인증 필터(jwt 토큰 필터)
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+        ;
 
         return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(){
+        return new JwtAuthenticationFilter(jwtTokenProvider, userDetailsService);
     }
 
 }
