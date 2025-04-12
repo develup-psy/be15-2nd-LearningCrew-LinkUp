2599362f10d46c03db480642ad59e3683f1b220c
PSY <develup-psy@github.com>
2025-04-12 14:23:32 +0900

    refactor: 이메일 관련 리팩토링

---
+++ linkup/src/main/java/com/learningcrew/linkup/linker/command/application/dto/request/ResetPasswordRequest.java
@@ -0,0 +1,27 @@
+package com.learningcrew.linkup.linker.command.application.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ResetPasswordRequest {
+
+    @Email
+    @NotBlank
+    @Schema(description = "이메일", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String email;
+
+    @NotBlank
+    @NotBlank
+    @Schema(description = "비밀번호 재설정 토큰", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String token;
+
+    @NotBlank
+    @NotBlank
+    @Schema(description = "새로운 비밀번호", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String newPassword;
+}
