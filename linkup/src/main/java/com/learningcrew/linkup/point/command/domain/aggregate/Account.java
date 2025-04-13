a26a348c68c4f9f9dbb21054ff9a9ec7098479eb
PSY <develup-psy@github.com>
2025-04-13 23:49:48 +0900

    feat: Account Entity 및 Repository 구현

---
+++ linkup/src/main/java/com/learningcrew/linkup/point/command/domain/aggregate/Account.java
@@ -0,0 +1,43 @@
+package com.learningcrew.linkup.point.command.domain.aggregate;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "account")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int accountId;
+
+    private int userId;
+
+    private String bankName;
+
+    private String accountNumber;
+
+    private String holderName;
+
+    private int status_id;
+
+    @CreatedDate
+    LocalDateTime createdAt;
+
+    public void update(String bankName, String accountNumber, String accountHolder) {
+        this.bankName = bankName;
+        this.accountNumber = accountNumber;
+        this.holderName = accountHolder;
+    }
+}
+
