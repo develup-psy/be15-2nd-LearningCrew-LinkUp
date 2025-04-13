3dfb907bcf3135adb3babb2a1bbd38bcccdd458f
PSY <develup-psy@github.com>
2025-04-13 12:22:59 +0900

    feat: 서비스 기능 구현 완료

--- linkup/src/main/java/com/learningcrew/linkup/linker/query/service/FriendQueryServiceImpl.java
+++ linkup/src/main/java/com/learningcrew/linkup/linker/query/service/FriendQueryServiceImpl.java
@@ -1,27 +1,37 @@
 package com.learningcrew.linkup.linker.query.service;
 
+import com.learningcrew.linkup.linker.query.dto.query.FriendInfoDTO;
 import com.learningcrew.linkup.linker.query.dto.response.FriendRequestResponse;
-import com.learningcrew.linkup.linker.query.dto.response.FriendResponse;
+import com.learningcrew.linkup.linker.query.dto.response.FriendInfoResponse;
+import com.learningcrew.linkup.linker.query.mapper.FriendMapper;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Schema
 @Service
+@RequiredArgsConstructor
 public class FriendQueryServiceImpl implements FriendQueryService {
+    private final FriendMapper friendMapper;
+    private final ModelMapper modelMapper;
+
     @Override
-    public List<FriendResponse> getFriends(int memberId) {
-        return List.of();
+    public List<FriendInfoResponse> getFriends(int memberId) {
+        // 친구 목록 조회(ACCEPTED 상태인 친구 관계만 조회)
+        List<FriendInfoDTO> friendList = friendMapper.findAcceptedFriends(memberId);
+
+        return friendList.stream().map(
+                (friendInfo) -> modelMapper.map(friendInfo, FriendInfoResponse.class)
+        ).toList();
     }
 
     @Override
     public List<FriendRequestResponse> getReceivedRequests(int adresseeId) {
-        return List.of();
-    }
 
-    @Override
-    public List<FriendRequestResponse> getSentRequests(int requesterId) {
         return List.of();
     }
 }
