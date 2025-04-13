ced87a3e9ef51981d19e290cf4b053db98d29864
PSY <develup-psy@github.com>
2025-04-13 13:05:56 +0900

    feat: 컨트롤러 구현

--- linkup/src/main/java/com/learningcrew/linkup/linker/query/controller/FriendQueryController.java
+++ linkup/src/main/java/com/learningcrew/linkup/linker/query/controller/FriendQueryController.java
@@ -7,6 +7,7 @@ import com.learningcrew.linkup.linker.query.service.FriendQueryService;
 import com.learningcrew.linkup.security.jwt.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/friends")
@@ -31,7 +33,9 @@ public class FriendQueryController {
     @GetMapping("/received")
     @Operation(summary = "받은 친구 요청 조회", description = "내가 받은 친구 요청 목록을 조회합니다.")
     public ResponseEntity<ApiResponse<List<FriendRequestResponse>>> getReceivedRequests(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        log.info("Get received friend requests list for user {}", customUserDetails.getUserId());
         List<FriendRequestResponse> responses = friendQueryService.getReceivedRequests(customUserDetails.getUserId());
+        log.info("받은 친구 요청 조회 완료");
         return ResponseEntity.ok(ApiResponse.success(responses, "받은 친구 요청 조회 성공"));
     }
 }
