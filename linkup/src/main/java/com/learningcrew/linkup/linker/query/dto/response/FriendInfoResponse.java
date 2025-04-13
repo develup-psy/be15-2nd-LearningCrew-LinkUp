152b00523e56d6f07156bf494ba1a359e6ac81d5
PSY <develup-psy@github.com>
2025-04-13 12:22:24 +0900

    feat: Mapper Query 및 Response DTO 구현

--- linkup/src/main/java/com/learningcrew/linkup/linker/query/dto/response/FriendInfoResponse.java
+++ linkup/src/main/java/com/learningcrew/linkup/linker/query/dto/response/FriendInfoResponse.java
@@ -1,14 +1,15 @@
-package com.learningcrew.linkup.linker.query.dto.query;
+package com.learningcrew.linkup.linker.query.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Builder
-public class FriendInfoDTO {
+@RequiredArgsConstructor
+@Schema(description = "친구 목록 조회 Response DTO ")
+public class FriendInfoResponse {
 
     @Schema(description = "친구 회원 ID", example = "8")
     private Integer friendId;
@@ -18,5 +19,8 @@ public class FriendInfoDTO {
 
     @Schema(description = "프로필 이미지 URL", example = "https://cdn.linker.com/profile/8.jpg")
     private String profileImageUrl;
+
+    @Schema(description = "친구 수락 시점", example = "2025-04-10T21:30:00")
+    private LocalDateTime connectedAt;
 }
 
