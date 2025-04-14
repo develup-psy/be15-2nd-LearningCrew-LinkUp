00753662d7f44dc31d0be1e2fe76b4a798708d72
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-04-14 20:56:09 +0900

    배치 작업을 위한 annotation 추가

--- linkup/src/main/java/com/learningcrew/linkup/LinkupApplication.java
+++ linkup/src/main/java/com/learningcrew/linkup/LinkupApplication.java
@@ -1,11 +1,11 @@
 package com.learningcrew.linkup;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LinkupApplication {
 
 	public static void main(String[] args) {
