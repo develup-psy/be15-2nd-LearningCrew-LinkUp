2f55ae9ae9666b33a35da4921731b10d0696e9e9
jihye25 <wisdom95725@gmail.com>
2025-04-17 19:45:48 +0900

    feat: test

---
+++ linkup/src/main/java/com/learningcrew/linkup/community/command/application/controller/CommentLikeController.java
@@ -0,0 +1,29 @@
+package com.learningcrew.linkup.community.command.application.controller;
+
+import com.learningcrew.linkup.common.dto.ApiResponse;
+import com.learningcrew.linkup.community.command.application.service.CommentLikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/comments/{commentId}/likes")
+@RequiredArgsConstructor
+public class CommentLikeController {
+
+    private final CommentLikeService commentLikeService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Void>> likeComment(@PathVariable int commentId,
+                                                         @RequestParam int userId) {
+        commentLikeService.likeComment(commentId, userId);
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<ApiResponse<Void>> unlikeComment(@PathVariable int commentId,
+                                                           @RequestParam int userId) {
+        commentLikeService.unlikeComment(commentId, userId);
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+}
